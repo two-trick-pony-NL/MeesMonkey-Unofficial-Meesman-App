e3be4e96bfda2df0fc3f3188ffe5b4e4261e8c4d
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2024-02-16 19:06:13 +0100

    Moved keyboardavoidingview

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
-import { View, Text,KeyboardAvoidingView, TextInput, Button, StyleSheet, ActivityIndicator, TouchableOpacity, Linking, Image } from 'react-native';
-import getAuthToken from '../api/AuthApi';
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TextInput,
+  Button,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+  Linking,
+  Image,
+} from "react-native";
+import getAuthToken from "../api/AuthApi";
 
 const LoginScreen = ({ onLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
@@ -15,7 +25,7 @@ const LoginScreen = ({ onLogin }) => {
       const token = await getAuthToken(lowercasedUsername, password);
       onLogin(token);
     } catch (error) {
-      alert('Login failed. Please check your credentials.');
+      alert("Login failed. Please check your credentials.");
     } finally {
       setLoading(false);
     }
@@ -24,12 +34,17 @@ const LoginScreen = ({ onLogin }) => {
   const handleWebLinkPress = () => {
     // Open the webpage when the subtext is pressed
     // Replace 'https://your-website.com' with your actual webpage URL
-    Linking.openURL('https://github.com/two-trick-pony-NL/MeesMonkey-Unofficial-Meesman-App');
+    Linking.openURL(
+      "https://github.com/two-trick-pony-NL/MeesMonkey-Unofficial-Meesman-App",
+    );
   };
 
   return (
-    <KeyboardAvoidingView behavior='padding' style={styles.container}>
-      <Image source={require('../assets/MeesmanIcon.png')} style={styles.logo} />
+    <View behavior="padding" style={styles.container}>
+      <Image
+        source={require("../assets/MeesmanIcon.png")}
+        style={styles.logo}
+      />
 
       <Text style={styles.title}>Login</Text>
       <TextInput
@@ -54,22 +69,17 @@ const LoginScreen = ({ onLogin }) => {
           <ActivityIndicator size="large" color="white" />
         </View>
       )}
-        <Text style={styles.subtextnormal}>Deze app wordt niet officieel onersteund door Meesman indexbeleggen. Gebruik is voor eigen risico. De die je hier invoert worden versleuteld op jouw telefoon bewaard.</Text>
-
-      <TouchableOpacity onPress={handleWebLinkPress}>
-        <Text style={styles.subtext}>Meer weten</Text>
-      </TouchableOpacity>
-    </KeyboardAvoidingView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
-    backgroundColor: '#384956',
+    backgroundColor: "#384956",
   },
   logo: {
     width: 250, // Adjust the width according to your design
@@ -78,46 +88,46 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 16,
-    color: '#fff',
+    color: "#fff",
   },
   input: {
     height: 40,
-    borderColor: 'white',
-    color: 'white',
+    borderColor: "white",
+    color: "white",
     borderWidth: 1,
     marginBottom: 16,
     paddingHorizontal: 10,
-    width: '100%',
+    width: "100%",
   },
   loadingContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     bottom: 0,
     left: 0,
     right: 0,
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#384956',
-    color: 'white',
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#384956",
+    color: "white",
   },
   loadingText: {
     marginTop: 10,
     fontSize: 16,
-    color: 'white',
+    color: "white",
   },
   subtext: {
     marginTop: 20,
     fontSize: 10,
-    color: 'lightgray',
-    textDecorationLine: 'underline',
+    color: "lightgray",
+    textDecorationLine: "underline",
   },
   subtextnormal: {
     marginTop: 20,
     fontSize: 10,
-    color: 'lightgray',
+    color: "lightgray",
   },
 });
 
